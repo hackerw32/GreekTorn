2e84c37f6dc2e0acebfeaa4563973733d5791537
George <george@example.com>
2026-04-09 23:13:35 +0300

    bug fix

--- src/stores/dailyRewardStore.js
+++ src/stores/dailyRewardStore.js
@@ -3,10 +3,7 @@ import { dailyRewards } from '../data/dailyRewards'
 import { usePlayerStore } from './playerStore'
 import { useInventoryStore } from './inventoryStore'
 import { useGameStore } from './gameStore'
-<<<<<<< HEAD
 import { usePropertyStore } from './propertyStore'
-=======
->>>>>>> 6804455d765069a685690bbb983b0915b965cd54
 
 export const useDailyRewardStore = defineStore('dailyReward', {
   state: () => ({
@@ -39,11 +36,8 @@ export const useDailyRewardStore = defineStore('dailyReward', {
 
   actions: {
     checkDaily() {
-<<<<<<< HEAD
       // Process daily rent/eviction cycle whenever daily systems refresh.
       usePropertyStore().checkDailyRent()
-=======
->>>>>>> 6804455d765069a685690bbb983b0915b965cd54
       if (this.canClaim) {
         this.pendingReward = true
       }
